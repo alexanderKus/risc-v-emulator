a5ec82b2fa6c05dc9b65db1115a0b22101238216
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2024-04-17 21:23:56 +0200

    Instruction Set codes

--- main.cc
+++ main.cc
@@ -5,142 +5,194 @@
 #include <stdexcept>
 #include <format>
 
-/*
- *  Memory Map
- *  ROM: 0x10000000
- *  RAM: 0x20000000
- */
 
-enum MemoryMap {
-  ROM_ADDR = 0x10000000,
-  RAM_ADDR = 0x20000000
+class Registers {
+public:
+  uint32_t x0 = 0;  // zero
+  uint32_t x1 = 0;  // ra - return addres
+  uint32_t x2 = 0;  // sp - stack pointer
+  uint32_t x3 = 0;  // gp - global pointer
+  uint32_t x4 = 0;  // tp - thread pointer
+  uint32_t x5 = 0;  // t0 - temporary
+  uint32_t x6 = 0;  // t1 - temporary
+  uint32_t x7 = 0;  // t2 - temporary
+  uint32_t x8 = 0;  // fp/s0 - save register / frame pointer
+  uint32_t x9 = 0;  // s1 - save register
+  uint32_t x10 = 0; // a0 - function argument / return value
+  uint32_t x11 = 0; // a1 - function argument / return value
+  uint32_t x12 = 0; // a2 - function arugment  
+  uint32_t x13 = 0; // a3
+  uint32_t x14 = 0; // a4
+  uint32_t x15 = 0; // a5
+  uint32_t x16 = 0; // a6
+  uint32_t x17 = 0; // a7
+  uint32_t x18 = 0; // s2 - saved register
+  uint32_t x19 = 0; // s3
+  uint32_t x20 = 0; // s4
+  uint32_t x21 = 0; // s5
+  uint32_t x22 = 0; // s6
+  uint32_t x23 = 0; // s7
+  uint32_t x24 = 0; // s8
+  uint32_t x25 = 0; // s9
+  uint32_t x26 = 0; // s10
+  uint32_t x27 = 0; // s11
+  uint32_t x28 = 0; // t3 - temporary
+  uint32_t x29 = 0; // t4
+  uint32_t x30 = 0; // t5
+  uint32_t x31 = 0; // t6
+  uint32_t pc = 0;
 };
 
-// NOTE: rom and ram sizes are the same. 
-#define ROM_SIZE 1024 * 1024
-#define RAM_SIZE 1024 * 1024 * 4
+enum {
+  OPCODE_LUI        = 0b00110111,
+  OPCODE_AUIPC      = 0b00010111,
+  OPCODE_JAL        = 0b01101111,
+  OPCODE_JALR       = 0b01100111,
+  OPCODE_BRANCH     = 0b01100011,
+  OPCODE_LOAD       = 0b00000011,
+  OPCODE_STORE      = 0b01000011,
+  OPCODE_INT_COMP_I = 0b00100011,
+  OPCODE_INT_COMP_R = 0b00110011,
+  OPCODE_FENCE      = 0b00001111,
+  // NOTE: figure out better name than R
+  OPCODE_R          = 0b01110011,
+};
 
-class ROM {
+enum {
+  FUNC3_JALR = 0b000,
+
+  // BRANCH
+  FUNC3_BEQ  = 0b000,
+  FUNC3_BNE  = 0b001,
+  FUNC3_BLT  = 0b100,
+  FUNC3_BGE  = 0b101,
+  FUNC3_BLTU = 0b110,
+  FUNC3_BGEU = 0b111,
+
+  //LOAD
+  FUNC3_LOAD_BYTE   = 0b000,
+  FUNC3_LOAD_HALF   = 0b000,
+  FUNC3_LOAD_WORD   = 0b000,
+  FUNC3_LOAD_BYTE_U = 0b000,
+  FUNC3_LOAD_HAFL_U = 0b000,
+
+  // STORE
+  FUNC3_STORE_BYTE = 0b000,
+  FUNC3_STORE_HALF = 0b001,
+  FUNC3_STORE_WORD = 0b010,
+
+  // INT_COMP_I
+  FUNC3_ADDI  = 0b000,
+  FUNC3_SLTI  = 0b010,
+  FUNC3_SLTIU = 0b011,
+  FUNC3_XORI  = 0b100,
+  FUNC3_ORI   = 0b110,
+  FUNC3_ANDI  = 0b111,
+  FUNC3_SLLI  = 0b001,
+  FUNC3_SRLI  = 0b101,
+  FUNC3_SRAI  = 0b101,
+
+  // INT_COMP
+  FUNC3_ADD  = 0b000,
+  FUNC3_SUB  = 0b000,
+  FUNC3_SLL  = 0b001,
+  FUNC3_SLT  = 0b010,
+  FUNC3_SLTU = 0b011,
+  FUNC3_XOR  = 0b100,
+  FUNC3_SRL  = 0b101,
+  FUNC3_SRA  = 0b101,
+  FUNC3_OR   = 0b110,
+  FUNC3_AND  = 0b111,
+
+  // FENCE
+  FUNC3_FENCE  = 0b000,
+  FUNC3_FENCEI = 0b001,
+
+  // R
+  FUNC3_SCALL      = 0b000,
+  FUNC3_SBREAK     = 0b000,
+  FUNC3_SRDCYCLE   = 0b000,
+  FUNC3_SRDCYCLEH  = 0b000,
+  FUNC3_RDTIME     = 0b000,
+  FUNC3_RDTIMEH    = 0b000,
+  FUNC3_RDINSTRET  = 0b000,
+  FUNC3_RDINSTRETH = 0b000,
+
+  // R
+  FUNC7_SCALL      = 0b000000000000,
+  FUNC7_SBREAK     = 0b000000000001,
+  FUNC7_SRDCYCLE   = 0b110000000000,
+  FUNC7_SRDCYCLEH  = 0b110010000000,
+  FUNC7_RDTIME     = 0b110000000001,
+  FUNC7_RDTIMEH    = 0b110010000001,
+  FUNC7_RDINSTRET  = 0b110000000010,
+  FUNC7_RDINSTRETH = 0b110010000010,
+};
+
+
+class Ram {
 private:
-  uint32_t _mask = (ROM_SIZE / 4) - 1;
-  std::vector<uint32_t> _rom;
-
+  uint32_t _data[1024 * 1024 * 4];
 public:
-  ROM() {
-    this->_rom.resize(ROM_SIZE / 4);
-  }
+  Ram() { }
 
   uint32_t read(uint32_t addr) const {
-    const uint32_t a = addr >> 2; // division by 4
-    auto value = this->_rom.at(a & _mask);
-    std::cout << std::format("Read  ROM addr 0x{:x} value 0x{:x}", addr, value) << std::endl;
-    return value;
+    return _data[addr];
   }
 
-  void load(std::vector<uint32_t> &data) {
-    for(uint32_t i = 0; i < ROM_SIZE / 4; i++) {
-        if(i >= data.size()) this->_rom.at(i) = 0xffffffff; 
-        else this->_rom.at(i) = data.at(i);
-      }
+  void write(uint32_t addr, uint32_t data) {
+    _data[addr] = data;
   }
 };
 
-class RAM {
+class Instruction {
 private:
-  uint32_t _mask = (RAM_SIZE / 4) - 1;
-  std::vector<uint32_t> _ram;
-
+  uint32_t _value;
 public:
-  RAM() {
-    this->_ram.resize(RAM_SIZE / 4);
+  Instruction(uint32_t value) {
+    this->_value = value;
   }
 
-  uint32_t read(uint32_t addr) const {
-    const uint32_t a = addr >> 2;
-    auto value = this->_ram.at(a & _mask);
-    std::cout << std::format("Read  RAM addr 0x{:x} value 0x{:x}", addr, value) << std::endl;
-    return value;
+  uint32_t getValue() const {
+    return this->_value;
   }
 
-  void write(uint32_t addr, uint32_t value) {
-    const uint32_t a = addr >> 2;
-    std::cout << std::format("Write RAM addr 0x{:x} value 0x{:x}", addr, value) << std::endl;
-    this->_ram.at(a & _mask) = value;
+  uint32_t getOpcode() const {
+    return this->_value & 0xfffffF82;
   }
 };
 
-// Interface for MMIO
-class MMIOSystem {
-public:
-  MMIOSystem(ROM &rom, RAM &ram) {
-    this->_rom = rom;
-    this->_ram = ram;
-  }
-
-  uint32_t read(uint32_t addr) const {
-    if ((addr & ROM_ADDR) == ROM_ADDR) {
-      return this->_rom.read(addr & 0x0fffffff);
-    }
-    else if ((addr & RAM_ADDR) == RAM_ADDR) {
-      return this->_ram.read(addr & 0x0fffffff);
-    }
-
-    throw std::invalid_argument("unhandled addr 0x:" + std::format("{:x}", addr));
-    return 0;
-  }
-
-  void write(uint32_t addr, uint32_t value) {
-    if ((addr & ROM_ADDR) == ROM_ADDR) {
-      // cannot write to ROM!
-    }
-    else if ((addr & RAM_ADDR) == RAM_ADDR) {
-      this->_ram.write(addr & 0x0fffffff, value);
-    }
-  }
-
-  // NOTE: this should not be allowed!!!
-  void load(std::vector<uint32_t> &data) {
-    this->_rom.load(data);
-  }
-
-private:
-  ROM _rom;
-  RAM _ram;
-};
-
-// RISC-V 32bit base instruction set
 class RV32I {
-public:
-  RV32I() {
-  }
+private:
+  Registers _regs;
 
-  ROM* rom = new ROM();
-  RAM* ram = new RAM();
-  MMIOSystem* bus = new MMIOSystem(*this->rom, *this->ram);
+
+  void instruction_fetch() {}
+  void instruction_decode() {}
+  void execute() {}
+  void memory_access() {}
+  void write_back() {}
+
+public:
+  RV32I() { }
+
+  /*
+   * Pipeline
+   * 1. IF (instruction fetch)
+   * 2. ID (instruction decode)
+   * 3. EX (execute)
+   * 4. MA (memory access)
+   * 5. WB (write back)
+   */
+  void run() {
+     
+  }
 };
 
 
 
 int main() {
   auto rv = new RV32I();
-
-  auto a = std::vector<uint32_t>(2, 0x1337);
-  rv->bus->load(a);
-  rv->bus->read(0x10000000);
-  rv->bus->read(0x10000004);
-
-  rv->bus->write(0x20001000, 0x1337);
-  rv->bus->read(0x20001000);
-
-/*
- * 1024 * 1024 = 0x00400000 end of memory
- * Start writing data at the beginnig
- *
- * rv->bus->write(0x20000000, 0x1337);
- * rv->bus->read(0x20000000);
- * rv->bus->write(0x20400000, 0x2137);
- * rv->bus->read(0x20000000);
- */
 
   return 0;
 }
